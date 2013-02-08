b403a134b9da7ca962894b89c12f23c710989177
Adam Mlocek <mlocek@yahoo.com>
2013-02-08 13:05:07 +0000

    Added nuget pkg and QuickStart script

--- src/Fmat.Numerics/Script.fsx
+++ src/Fmat.Numerics/Script.fsx
@@ -1,12 +1,18 @@
-﻿#r @".\bin\debug\Fmat.Numerics.dll"
+﻿#r @".\bin\release\Fmat.Numerics.dll"
 open Fmat.Numerics
 open Fmat.Numerics.Conversion
-open Fmat.Numerics.NumericLiteralG
-open Fmat.Numerics.Matrix32Functions
+open Fmat.Numerics.MatrixFunctions
 open Fmat.Numerics.BasicStat
 open Fmat.Numerics.LinearAlgebra
 open System
-open System.Collections.Generic
+
+
+
+
+
+
+
+
 
 
 
